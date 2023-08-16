640801beeaa36a88dc609fa94ae30efba81efa8a
Titus <dev@tituscoleman.com>
2023-08-16 14:17:14 -0400

    fix updateReport validation logic

--- src/utils/reportTypeValidator.ts
+++ src/utils/reportTypeValidator.ts
@@ -1,16 +1,16 @@
 import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
 
-export function IsValidReportType(values: string[]) {
+export function IsValidReportType(allowedValues: string[]) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
       name: 'isValidReportType',
       target: object.constructor,
       propertyName: propertyName,
-      constraints: [values],
+      constraints: allowedValues,
       options: {message: 'Must be a valid report type: expense or income (all lowercase)'},
       validator: {
         validate(value: any, args: ValidationArguments) {
-            return args.constraints.includes(values);
+            return args.constraints.includes(value);
         }
       },
     });
